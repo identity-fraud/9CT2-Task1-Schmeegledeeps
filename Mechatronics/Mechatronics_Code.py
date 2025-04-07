b1fce4ec1f361d67b1faf88b6f246d3dd3dadfff
Tunip-Anip <109420023+Tunip-Anip@users.noreply.github.com>
2025-04-07 13:28:13 +1000

    Update Mechatronics_Code.py

--- Mechatronics/Mechatronics_Code.py
+++ Mechatronics/Mechatronics_Code.py
@@ -14,11 +14,16 @@ left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-
+mapx = 400
+mapy = 1800
 ev3.speaker.beep()
 
 while True:
     robot.drive(200, 0)
+    robot.turn(-90)
+    robot.straight(mapx/2)
+    robot.turn(-90)
+    robot.straight(mapy)
 
     while obstacle_sensor.distance() > 300:
         wait(10)
