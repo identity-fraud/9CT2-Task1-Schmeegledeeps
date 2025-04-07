8b8281f92f9856bf96c796dd3c5b852033b2f6ff
Tunip-Anip <109420023+Tunip-Anip@users.noreply.github.com>
2025-04-07 13:21:53 +1000

    Create Mechatronics_Code.py

---
+++ Mechatronics/Mechatronics_Code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, UltrasonicSensor
+from pybricks.parameters import Port
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase
+
+ev3 = EV3Brick()
+
+obstacle_sensor = UltrasonicSensor(Port.S4)
+
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+ev3.speaker.beep()
+
+while True:
+    robot.drive(200, 0)
+
+    while obstacle_sensor.distance() > 300:
+        wait(10)
+
+    robot.straight(-300)
+    robot.turn(120)
