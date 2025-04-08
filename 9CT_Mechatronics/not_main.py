f971f45c237add0193f08f17f9dca423c27b8c09
transaction-fraud <william.kim22@education.nsw.gov.au>
2025-04-08 11:07:29 +1000

    Update not_main.py
    iiiiiii

--- 9CT_Mechatronics/not_main.py
+++ 9CT_Mechatronics/not_main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.ev3devices import *
 from pybricks.parameters import Port
-from pybricks.tools import wait
+from pybricks.tools import *
 from pybricks.robotics import DriveBase
 
 ev3 = EV3Brick()
@@ -19,28 +19,19 @@ obstacle_sensor = UltrasonicSensor(Port.S4)
 #Combines variables/motors together
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-correct_colours = []
+correct_colors = ["RED", "YELLOW"]
 
 def main():
 
-
-
     while True:
-        if color == white:
-            robot.turn(360)
-            robot.straight(100)
-        elif color == black:
-            robot.straight(100)
-        
-
-while True:
-    robot.drive()
-    if color_sensor.reflection() <= 10:
-        robot.stop()
-        robot.turn(90)
-    if obstacle_sensor.distance() >= 100:
-        robot.stop()
-        robot.turn(90)
-    if color_sensor.color() == 
+        robot.drive()
+        if color_sensor.reflection() <= 10:
+            robot.stop()
+            robot.turn(90)
+        elif obstacle_sensor.distance() >= 100:
+            robot.stop()
+            robot.turn(90)
+            
+            
 
     
