4a63b40e8e7d1aba1efd0051247a697c611405f3
transaction-fraud <william.kim22@education.nsw.gov.au>
2025-04-08 12:21:02 +1000

    Update not_main.py

--- 9CT_Mechatronics/not_main.py
+++ 9CT_Mechatronics/not_main.py
@@ -12,6 +12,7 @@ left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 color_sensor = ColorSensor(Port.S3)
+color_sensor_down = ColorSensor(Port.S1)
 color = color_sensor.color()
 
 obstacle_sensor = UltrasonicSensor(Port.S4)
@@ -21,17 +22,52 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
 correct_colors = ["RED", "YELLOW"]
 
+def goBack():
+    
+    while True:
+
+        robot.drive()
+        if color_sensor_down.reflection() <= 10:
+            robot.stop()
+            robot.turn(90)
+        
+        if obstacle_sensor.distance() >= 100:
+            robot.stop()
+            robot.turn(90)
+
+        if color_sensor_down.color() == 'BLUE':
+            robot.stop()
+            # Drop correct object
+            if len(correct_colors) == 0:
+                break
+    
+    raise SystemExit
+              
+
+
+
 def main():
 
     while True:
+
         robot.drive()
-        if color_sensor.reflection() <= 10:
+
+        if color_sensor_down.reflection() <= 10:
+            robot.stop()
+            robot.turn(100) # Value is 100 so it does not loop around the mat
+
+        if obstacle_sensor.distance() >= 100:
             robot.stop()
             robot.turn(90)
-        elif obstacle_sensor.distance() >= 100:
+        
+        if color in correct_colors:
+            correct_colors.remove(color)
             robot.stop()
-            robot.turn(90)
+            # Pickup the correct object
+            goBack()
+
             
-            
+main() 
+# Thanks for all the fish           
 
     
