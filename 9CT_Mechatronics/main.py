e59fccca1462eff7b6a5a32c00484b54bc509138
identity-fraud <william.kim22@education.nsw.gov.au>
2025-05-13 12:29:02 +1000

    Grabber motor update

--- 9CT_Mechatronics/main.py
+++ 9CT_Mechatronics/main.py
@@ -3,15 +3,16 @@
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Port
-# from pybricks.tools import (wait, Stopwatch, DataLog) - Not used
+from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 
 # Note that this code cannot be run in this directory, and can only be run from a seperate one
 ev3 = EV3Brick()
 
-#Adds variables to motor
+#Adds variables to each sensor
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
+grab_motor = Motor(Port.S2)
 
 color_sensor = ColorSensor(Port.S3)
 color_sensor_down = ColorSensor(Port.S1)
@@ -34,7 +35,7 @@ def goBack():
             robot.turn(90) # Alternatively turns 90 degrees so that it can loop around the mat
         if color_sensor_down.color() == 'BLUE': # On the mat, there are blue lines that indicate the start area
             robot.stop 
-            # Drop object
+            robot.run(-100) # Negative integer so that it unclamps on the correct block
             if len(correct_colors) == 0: # If there are no more correct colour blocks, finish program
                     robot.stop
                     break
@@ -61,6 +62,11 @@ def main(): # Beginning of the main program
 
         if color_sensor.color() in correct_colors: # If the colour that it detects is in the correct_colors list, picks up the object
             correct_colors.remove(color) 
+            robot.stop()
+            robot.run(100) # Moves the motor into the correct blocks
+            wait(1000)
+            robot.brake()
+
             # Pickup the correct object
             break 
     
